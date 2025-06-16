6d7868be4b123848ef74ddf7bdd574ab5259a747
Hien <74279944+hihihien@users.noreply.github.com>
2025-06-16 11:49:34 +0200

    Update parse_pdf.py

--- parse_pdf.py
+++ parse_pdf.py
@@ -5,38 +5,39 @@ import re
 import json
 from pathlib import Path
 
-print("script started")
-
 PDF_PATH = "data/Modulhandbuch_MMI_V1-0.pdf"
-OUTPUT_PATH ="data/modules.json"
+OUTPUT_PATH = "data/modules.json"
 
+#read all text
 doc = fitz.open(PDF_PATH)
 all_text = ""
 for page in doc:
     all_text += page.get_text()
 
+#split module by patterns, up to the start of the next MMI or end
 pattern = re.compile(r"(MMI \d{2}(?:\.\d{2})?: .*?)(?=MMI \d{2}(?:\.\d{2})?: |\Z)", re.DOTALL)
 matches = pattern.findall(all_text)
 
 modules = []
 
 for raw in matches:
-    #extract module ID and title
+    # Extract module ID and title
     header_match = re.match(r"(MMI \d{2}(?:\.\d{2})?): (.*)", raw)
     if not header_match:
         continue
     module_id = header_match.group(1).strip()
     title = header_match.group(2).split("\n")[0].strip()
 
-    #save the chunk
+    # Save the chunk
     modules.append({
         "module_id": module_id,
         "title": title,
         "content": raw.strip()
     })
 
+#save
 Path("data").mkdir(exist_ok=True)
 with open(OUTPUT_PATH, "w", encoding="utf-8") as f:
     json.dump(modules, f, indent=2, ensure_ascii=False)
 
-print(f"sucessfully extracted and saved {len(modules)} modules to {OUTPUT_PATH}")
+print(f"Extracted and saved {len(modules)} modules to {OUTPUT_PATH}")
